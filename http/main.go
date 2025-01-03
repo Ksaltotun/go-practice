5d23a22548ad787ab40b21bb134af9d8be8bc2fd
Mishchenko.R <Mishchenko.R@mintrud.by>
2025-01-03 15:17:53 +0300

    add new info

--- http/main.go
+++ http/main.go
@@ -5,21 +5,29 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		const prefix string = "http://"
+		if !strings.HasPrefix(url, prefix) {
+			url = prefix + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v\n", err)
 			os.Exit(1)
 		}
-		b, err := io.ReadAll(resp.Body)
+		//b, err := io.ReadAll(resp.Body)
+		b, err := io.Copy(os.Stdout, resp.Body)
+
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v\n", err)
 			os.Exit(1)
 		}
+		fmt.Println("Answer status is: ........", resp.Status)
 		fmt.Println(string(b))
 	}
 }
